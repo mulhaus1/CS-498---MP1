4161d40e75a82b4f0dae9697698b206b75372355
unknown <Mike@.(none)>
2013-02-12 01:43:38 -0600

    added in functionality for removing list items, but still need to remove from list object array

--- jScript.js
+++ jScript.js
@@ -2,39 +2,49 @@
  * @author Mike
  */
 
-i = 0;
+// i = 0;
+//
+// function addItem (form) {
+// var textval = form.inputText.value;
+// var buttonId = "btn" + i;
+// var listId = "item" + i;
+// i++;
+//
+// $("#myList").append('<li id="' + listId + '">' + textval + '</li>');
+// var test = $('<input />', {
+// type: 'button',
+// name: buttonId,
+// value: 'delete',
+// onclick: 'deleteItem()'
+// });
+// $("#myList").append(test);
+// $('input[name="inputText"]').val('');
+// }
 
-function addItem (form) {
-	// var textval = form.inputText.value;
-	// var buttonId = "btn" + i;
-	// var listId = "item" + i;
-	// i++;
-// 	
-	// $("#myList").append('<li id="' + listId + '">' + textval + '</li>');
-	// var test = $('<input />', {
-					// type: 'button',
-					// name: buttonId,
-					// value: 'delete',
-					// onclick: 'deleteItem()'
-			// });
-	// $("#myList").append(test);
-	//$('input[name="inputText"]').val('');
-}
+var itemList = [];
+$('input[name="button"]').click(function() {
+	itemList.push($('input[name="inputText"]').val())
+	$("#myList").empty();
+	$.each(itemList, function() {
+		$("#myList").append('<li name = "listItem"><p>' + this + '</p></li>');
+	});
+	$('li[name="listItem"]').append('<input type="button" name="btnRemove" class="Remove" value="Remove">');
+	$('li[name="listItem"]').append('<input type="button" name="btnAdd" class="Add" value="Add Item">');
 
-$(document).ready(function() {
-	var itemList = [];
-	$('input[name="button"]').click(function() {
-		itemList.push($('input[name="inputText"]').val())
-		$("#myList").empty();
-		$.each(itemList, function() {
-			$("#myList").append('<li name = "listItem"><p>' + this + '</p></li>');
-		});
-		$('li[name="listItem"]').append('<input type="button" name="btnRemove" value="Remove Item">');
-		$('li[name="listItem"]').append('<input type="button" name="btnAdd" value="Add Item">');
-		
-		$('input[name="inputText"]').val('');
+	$('input[name="inputText"]').val('');
+});
+
+$('#myList').on("click", ".Remove", function() {
+	var item = ($(this).closest("li"));
+	item.hide(100, 'swing', function(){
+		item.remove();
+		console.log(item);
 	});
-	$('img[name="imgClose"]').click(function() {
-		
-	});
+	//text = item.
+	//var newList = itemList.grep();
+});
+
+$('#myList').on("click", ".Add", function() {
+	
+	console.log("adding list item");
 });
